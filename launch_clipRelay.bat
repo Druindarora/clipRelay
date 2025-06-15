9dfb1414f20d1db2498734c54112eca50670c543
Druindarora <druindar.inarora@protonmail.com>
2025-06-15 10:54:19 +0200

    Retire les fichiers locaux du suivi pour qu'ils soient ignorés

--- launch_clipRelay.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Active l'environnement (à adapter à ton chemin)
-@REM call D:\venvs\whisper-env\Scripts\activate.bat
-call c:\Tools\whisper-env\Scripts\activate.bat
-
-REM Lance ton script (à adapter aussi)
-python main.py
-
-@REM pause
